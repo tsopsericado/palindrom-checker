0c2c89e1c035554d8e87cc9d3ff1b25308b240bd
ricardo <ricardotsopse@gmail.com>
2023-11-10 22:38:58 +0100

    some ui changes made

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 let clearbtn = document.getElementById("clearbtn");
 function checkPal() {
   let str = document.getElementById("input").value;
- 
-  // remove any non alphanumerique and join the words to form one 
-   let splijoint = str.split(" ").join("")
-   console.log(splijoint)
+
+  // remove any non alphanumerique and join the words to form one
+  let splijoint = str.split(" ").join("");
+  console.log(splijoint);
 
   // looping through half of the string
   let isPlandrome = true;
   for (let i = 0; i < splijoint.length / 2; i++) {
-
     //checking if the first and last string are thesame.
     if (splijoint[i] !== splijoint[splijoint.length - i - 1]) {
       isPlandrome = false;
       break;
     }
   }
-  
+
   // let isPlandrome = true;
   let result = document.getElementById("result");
   if (isPlandrome) {
     result.innerHTML = "Yesss, sentence is a plaindrome";
   } else {
     result.innerHTML = "Nooo, sentence is not a plaindrome";
-  } 
+  }
 
   // adding clear btn
   clearbtn.addEventListener("click", function () {
